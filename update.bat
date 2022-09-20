ea69486d63d6c4586d1b0860e8d1894c56abbf80
Kevin Owino <kowino@STIMA-SAC.COM>
2022-09-20 13:20:56 +0300

    First

---
+++ update.bat
@@ -0,0 +1,8 @@
+@echo off
+for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
+set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
+set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
+
+set "fullstamp=%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%"
+echo git add . && git commit -am "Git Push - %YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%" && git push origin master
+pause
